af18d5ffc646882df264c452d68212e6b39bcd6f
Jordan Gaza <Gaza>
2020-01-05 18:04:15 -0500

    Adding fucntion to get mouse position

--- script.js
+++ script.js
@@ -19,3 +19,18 @@ function setMousePosition(e) {
 }
 
 update();
+
+function getPosition(el) {
+    var xPosition = 0;
+    var yPosition = 0;
+   
+    while (el) {
+      xPosition += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+      yPosition += (el.offsetTop - el.scrollTop + el.clientTop);
+      el = el.offsetParent;
+    }
+    return {
+      x: xPosition,
+      y: yPosition
+    };
+  } 
