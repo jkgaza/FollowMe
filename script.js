2b301dafb2d323e0077b8093a3eeb4089b54c55d
Jordan Gaza <Gaza>
2020-01-05 18:24:13 -0500

    Added clear.Rect to clear pixels as circles moves

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var canvas = document.querySelector("#myCanvas");
 var context = canvas.getContext("2d");
 
 function update() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
   context.beginPath();
   context.arc(mouseX, mouseY, 50, 0, 2 * Math.PI, true);
   context.fillStyle = "#FF6A6A";
